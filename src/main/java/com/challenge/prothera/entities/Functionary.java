8a091ec6d17d4989b70ab8bc12449bd56c84f905
unknown <jefferson.crn@gmail.com>
2024-07-03 19:45:37 -0300

    feat: person and functionary entity

---
+++ src/main/java/com/challenge/prothera/entities/Functionary.java
@@ -0,0 +1,78 @@
+package com.challenge.prothera.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_functionary")
+public class Functionary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String office;
+    @Column
+    private Double salary;
+
+    @OneToOne()
+    @MapsId
+    private Person person;
+
+    public Functionary() {
+
+    }
+
+    public Functionary(Long id, String office, Double salary, Person person) {
+        this.id = id;
+        this.office = office;
+        this.salary = salary;
+        this.person = person;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOffice() {
+        return office;
+    }
+
+    public void setOffice(String office) {
+        this.office = office;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    @Override
+    public String toString() {
+        return "Functionary [id Functionary = " + id + ", office=" + office + ", salary=" + salary
+                + ", person=" + person + "]";
+    }
+
+}
---
+++ src/main/java/com/challenge/prothera/entities/Person.java
@@ -0,0 +1,77 @@
+package com.challenge.prothera.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_person")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private LocalDate birthDate;
+
+    @OneToOne(mappedBy = "person", cascade = CascadeType.ALL)
+    private Functionary functionary;
+
+    public Person() {
+
+    }
+
+    public Person(String name, LocalDate birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Functionary getFunctionary() {
+        return functionary;
+    }
+
+    public void setFunctionary(Functionary functionary) {
+        this.functionary = functionary;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [id pessoa=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
+    }
+
+}
