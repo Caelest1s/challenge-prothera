8b4f4dc540ce187d9196096a6747a5f09296feee
unknown <jefferson.crn@gmail.com>
2024-07-03 20:05:39 -0300

    feat: add import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,14 @@
+INSERT INTO tb_person(name, birth_date) VALUES ('Clara', TIMESTAMP WITH TIME ZONE '1996-06-03T13:00:00Z');
+INSERT INTO tb_functionary (person_id, salary, office) VALUES (1, 10000, 'Advogada');
+
+INSERT INTO tb_person(name, birth_date) VALUES ('Jefferson', TIMESTAMP WITH TIME ZONE '1989-12-18T15:00:00Z');
+INSERT INTO tb_functionary (person_id, salary, office) VALUES (2, 8000, 'Desenvolvedor');
+
+INSERT INTO tb_person(name, birth_date) VALUES ('Alexandre', TIMESTAMP WITH TIME ZONE '2026-09-22T18:00:00Z');
+INSERT INTO tb_functionary (person_id, salary, office) VALUES (3, 6000, 'CEO');
+
+INSERT INTO tb_person(name, birth_date) VALUES ('Anastacia', TIMESTAMP WITH TIME ZONE '2028-10-10T21:00:00Z');
+INSERT INTO tb_functionary (person_id, salary, office) VALUES (4, 4000, 'Finanças');
+
+INSERT INTO tb_person(name, birth_date) VALUES ('Clarice', TIMESTAMP WITH TIME ZONE '2029-04-13T22:00:00Z');
+INSERT INTO tb_functionary (person_id, salary, office) VALUES (5, 2000, 'Médica');
